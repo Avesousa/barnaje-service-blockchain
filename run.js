1ed0a019e1d73b6152afd8b8eb7af79f9668d4c4
Avelino Figueira <avesousa14@gmail.com>
2023-08-14 08:51:58 -0300

    feat: Add logic with user-genesis

--- run.js
+++ run.js
@@ -1,18 +1,32 @@
 const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
+const apiSecret = fs.readFileSync(".apiSecretWallet").toString().trim();
+
+// Read file .csv
+const users_genesis = require('./resource/output.json');
+
 
 // Define the provider
-const provider = "http://localhost:7545";
+const provider = "https://data-seed-prebsc-1-s1.bnbchain.org:8545";
+// const provider = "http://localhost:7545";
 const web3 = new Web3(new Web3.providers.HttpProvider(provider));
 
+// Define the function  
 async function run() {
-    // Use the first account as the default account
-    const accounts = await web3.eth.getAccounts();
-
     // Read the compiled contract (the JSON output from the Solidity compiler)
     const contractPath = path.resolve(__dirname, 'build/contracts/Barnaje.json');
     const contractJson = JSON.parse(fs.readFileSync(contractPath, 'utf8'));
+    
+    // Wallet developer
+    const account = await web3.eth.accounts.privateKeyToAccount(apiSecret);
+    const dev = account.address;
+    web3.eth.accounts.wallet.add(account);
+    console.log('Wallet developer ', dev);
+
+    // const accounts = await web3.eth.getAccounts();
+    // const dev = accounts[0]; 
+    // console.log("Wallet developer ", accounts);
 
     // Get the deployed address from the Truffle Contract JSON
     const networkId = await web3.eth.net.getId();
@@ -24,37 +38,42 @@ async function run() {
     // Create a function for view users
     const users = async (user) => console.log('User ', user, await contract.methods.getUser(user).call());
     const treeNode = async (user) => console.log('Tree node ', user, await contract.methods.getTree(user).call());
-    // Get the default account
-    const dao = accounts[1];
 
     // Call your contract's methods
-    await contract.methods.initialize().call({ from: dao });
-    await contract.methods.completeGenesis.call({ from: dao });
+    // await contract.methods.initialize().call({ from: dao });
+    // await contract.methods.completeGenesis.call({ from: dev });
 
     // Call the tree node function
-    // const users_genesis = [{me:accounts[1], balance: 67350, sponsor: '0x0000000000000000000000000000000000000000'},{me:accounts[0], balance: 67350, sponsor: accounts[1]},{me:accounts[2], balance: 67350, sponsor: accounts[0]},{me:accounts[3], balance: 1850, sponsor: accounts[2]},{me:accounts[4], balance: 67350, sponsor: accounts[0]},{me:accounts[5], balance: 21350, sponsor: accounts[3]},{me:accounts[6], balance: 67350, sponsor: accounts[0]},{me:accounts[7], balance: 13850, sponsor: accounts[0]},{me:accounts[8], balance: 13850, sponsor: accounts[0]},{me:accounts[9], balance: 6050, sponsor: accounts[4]}];
-    // const users_genesis = [
-    //     { me: accounts[1], balance: 67350, sponsor: '0x0000000000000000000000000000000000000000' }, 
-    //     { me: accounts[0], balance: 67350, sponsor: accounts[1] }, 
-    //     { me: accounts[2], balance: 67350, sponsor: accounts[0] }, 
-    //     { me: accounts[3], balance: 1850, sponsor: accounts[2] }, 
-    //     { me: accounts[4], balance: 67350, sponsor: accounts[0] }, 
-    //     { me: accounts[5], balance: 21350, sponsor: accounts[3] }, 
-    //     { me: accounts[6], balance: 67350, sponsor: accounts[0] }, 
-    //     { me: accounts[7], balance: 13850, sponsor: accounts[0] }, 
-    //     { me: accounts[8], balance: 13850, sponsor: accounts[0] }, 
-    //     { me: accounts[9], balance: 6050, sponsor: accounts[4] }
-    // ];
-
-    // for (let i = 0; i < users_genesis.length; i++) {
-    //     const user = users_genesis[i];
-    //     console.log('User ', user.me, ' run');
-    //     await contract.methods.completeUser(user.me, user.balance, user.sponsor).send({ from: dao, gas: 5000000 })
-    //         .then(() => {
-    //             console.log('User ', user.me, ' initialized');
-    //             // users(user.me);
-    //         })
-    //         .catch(e => console.error('User initialized error ', e));
+    for (let i = 0; i < users_genesis.length; i++) {
+        const user = users_genesis[i];
+        console.log('[',user.order, '] User ', user.wallet, ' creating...');
+        await contract.methods.completeUser(user.wallet, web3.utils.toWei(user.balance,'mwei'), user.wallet_sponsor, user.wallet_partner, user.wallet_left_child, user.wallet_right_child)
+        .send({ from: dev, gas: 5000000, gasPrice: web3.utils.toWei('20', 'gwei') })
+        .then((_res) => {
+            console.log("User ok");
+            console.log('[',user.order,'] User ', user.wallet, ' created');
+        })
+        .catch((e) => {
+            console.log("User error", e);
+            console.error( '[',e.data?.hash || e.hijackedStack,'] User ', user.wallet, e.data?.message || '', e.data?.reason || '');
+            throw e.data?.reason ? `${e.data.message} ${e.data.reason}` : e.hijackedStack;
+        });
+        console.log('User ', user.wallet, ' donating...');
+        await contract.methods.completeDonation(user.wallet)
+        .send({ from: dev, gas: 5000000, gasPrice: web3.utils.toWei('20', 'gwei') })
+        .then((_res) => {
+            console.log('[',user.order,'] User ', user.wallet, ' donated');
+        })
+        .catch((e) => {
+            console.error( '[',e.data?.hash || e.hijackedStack,'] User ', user.wallet, e.data?.message || '', e.data?.reason || '', 'Error', e);
+            throw e.data?.reason ? `${e.data.message} ${e.data.reason}` : e.hijackedStack;
+        });
+        users(user.wallet);
+        treeNode(user.wallet);
+    }
+    // const convertWeiToUSDT = (wei) => {
+    //     let balanceUSDTBigNumber = new web3.utils.BN(wei);
+    //     return parseFloat(web3.utils.fromWei(balanceUSDTBigNumber, 'mwei'));
     // }
 
 }
